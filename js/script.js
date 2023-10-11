cea8dd78cd7c34987790c8ffc5c6d8f4dd8606ff
mehran <mehransheyhki11@yahoo.com>
2023-10-11 18:14:16 +0330

    add dark mod to project

--- js/script.js
+++ js/script.js
@@ -20,3 +20,66 @@ function openSlider(){
 }
 sliderBackDropElem.addEventListener('click', closeSlider)
 window.addEventListener('load', sliderHandler);
+
+// ================== light & dark mode handller ========================
+let themeToggleDarkIcon = $.getElementById('theme-toggle-dark-icon');
+let themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+let themeToggleBtn = document.getElementById('theme-toggle');
+let themeColor;
+
+// Change the icons inside the button based on previous settings
+if (localStorage.getItem('slang-them-color') === 'dark' || (!('slang-them-color' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    themeToggleLightIcon.classList.remove('hidden');
+} else {
+    themeToggleDarkIcon.classList.remove('hidden');
+}
+
+themeToggleBtn.addEventListener('click', function() {
+
+    // toggle icons inside button
+    themeToggleDarkIcon.classList.toggle('hidden');
+    themeToggleLightIcon.classList.toggle('hidden');
+
+    // if set via local storage previously
+    if (localStorage.getItem('slang-them-color')) {
+        if (localStorage.getItem('slang-them-color') === 'light') {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('slang-them-color', 'dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('slang-them-color', 'light');
+        }
+
+    // if NOT set via local storage previously
+    } else {
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('slang-them-color', 'light');
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('slang-them-color', 'dark');
+        }
+    }
+    
+});
+
+function getThemeColor(){
+    return localStorage.getItem('slang-them-color')
+}
+
+function setThemeColor(){
+    themeColor = getThemeColor();
+    if(themeColor){
+        if(themeColor === 'dark' ){
+            document.documentElement.className = 'dark';
+            themeToggleLightIcon.classList.remove('hidden');
+            themeToggleDarkIcon.classList.add('hidden');
+        }else{
+            document.documentElement.className = 'light';
+            themeToggleDarkIcon.classList.remove('hidden');
+            themeToggleLightIcon.classList.add('hidden');
+        }
+    }
+    return
+}
+window.addEventListener('load', setThemeColor)
